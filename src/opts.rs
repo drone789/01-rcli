76cf2585c8888319894e8f1151ec80fbbe9b7253
drone789 <drone789@163.com>
2024-05-10 11:28:15 +0800

    support csv to json and yaml output.

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fmt, path::Path, str::FromStr};
 
 use clap::Parser;
 
@@ -14,14 +14,21 @@ pub enum SubCommand {
     #[command(name = "csv", about = "Show CSV , or Convert CSV to other formats")]
     Csv(CsvOps),
 }
-
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
 #[derive(Debug, Parser)]
 pub struct CsvOps {
     #[arg(short, long, default_value = "input.csv",value_parser = verify_file_exists)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")] // "output.json".into()
-    pub output: String,
+    #[arg(short, long)] // "output.json".into()
+    pub output: Option<String>,
+
+    #[arg(long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
 
     #[arg(long, default_value_t = true)]
     pub header: bool,
@@ -35,5 +42,40 @@ fn verify_file_exists(file_name: &str) -> Result<String, String> {
         Ok(file_name.into())
     } else {
         Err(format!("File {} does not exist", file_name))
+    }
+}
+
+fn parse_format(format_str: &str) -> Result<OutputFormat, anyhow::Error> {
+    // 调用了parse()方法，用于将字符串解析为指定的类型。在这里，它试图将format_str解析为OutputFormat类型
+    // 告诉编译器要将字符串解析为OutputFormat类型的值
+    format_str.parse::<OutputFormat>()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+            // OutputFormat::Toml => "toml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            // "toml" => Ok(OutputFormat::Toml),
+            _ => Err(anyhow::anyhow!("UnSupported format.")),
+        }
+    }
+}
+// into使用
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::opts::OutputFormat;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "PascalCase")]
@@ -17,19 +20,34 @@ struct Player {
     kit: u8,
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut ret = Vec::with_capacity(128);
 
-    for result in reader.deserialize() {
-        let record: Player = result?;
-        ret.push(record);
+    // for result in reader.deserialize() {
+    //      let record: Player = result?;
 
-        // println!("{:#?}", record);
+    //     ret.push(record);
+
+    // }
+
+    let headers = reader.headers()?.clone(); //读取表头
+    for result in reader.records() {
+        let record = result?;
+        // zip 将两个迭代器合并为元组迭代器[(header, record),...]
+        // collect 将元组迭代器转换为 Vec<(header, record)>
+
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        ret.push(json_value);
     }
 
-    let json = serde_json::to_string_pretty(&ret)?;
-    std::fs::write(output, json)?;
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    // let json = serde_json::to_string_pretty(&ret)?;
+    std::fs::write(output, content)?;
 
     Ok(())
 }
